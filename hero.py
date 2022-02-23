eb69ee2486a495927275d3d53671157169678410
Pasonik <p-ser-pan@yandex.ru>
2022-02-24 00:01:29 +0300

    Var_replaced_by_dict

--- hero.py
+++ hero.py
@@ -2,71 +2,70 @@ from random import randint
 
 # Создал класс героев
 class Hero:
-    # Шаблон персонажа(лвл ни на что не влияет)
-    def __init__(self, name, health, armor, power, weight, agility, critical_rate):
+    # Шаблон персонажа (лвл ни на что не влияет)
+    def __init__(self, name, health, armor, atk, weight, agility, critical_rate):
         self.name = name
         self.health = health + 15000
         self.armor = armor + 500
-        self.power = power + 1500
+        self.atk = atk + 1500
         self.weight = weight + 50
         self.agility = agility
         self.critical_rate = critical_rate
-        ''''# Общая сила учитывая силу оружия
-        self.apower = self.power + weapon.atk
-        # Подсчет ловкости засчепт параметров веса и скорости атаки оружия
-        self.speed = (1 - ((self.armor / 10 + weapon.w_weight) / 100)) * weapon.atk_speed
-        # Присвоил персонажу криты, чтобы не переписывать 2 раза функцию атаки для 2 персов
-        self.crit_rate = weapon.crit_rate
-        self.crit = weapon.crit'''
 
     def stats(self, weapon):
-        self.aweight = self.weight + weapon.w_weight
-        self.apower = self.power + weapon.atk
-        self.aagility = self.agility+((1-((self.armor/30+weapon.w_weight+self.weight/5)/100))*weapon.atk_speed)
-        self.crit_rate = self.critical_rate + weapon.crit_rate
-        self.crit = weapon.crit
-
+        self.aweight = self.weight + weapon.ws['weight']
+        self.aatk = self.atk + weapon.ws['atk']
+        self.aagility = self.agility + ((1 - ((self.armor / 30 + self.aweight / 5) / 100)) * weapon.ws['atk_speed'])
+        self.crit_rate = self.critical_rate + weapon.ws['crit_rate']
+        self.crit = weapon.ws['crit']
+        self.hs = {'name': self.name, 'health': self.health, 'armor': self.armor, 'all_atk': self.aatk,
+                   'all_weight': self.aweight, 'all_agility': self.aagility, 'crit': self.crit,
+                   'crit_rate': self.crit_rate}
 
     def print_info(self, weapon):
         # Инфа о созданном герое
-        print('Овации для сегодняшнего героя', self.name, str(randint(80,90)) + '-го уровня')
-        print('Оружие:', weapon.w_name)
-        print('Уровень здоровья:', self.health)
-        print('Сила героя:', self.apower)
-        print('С уровнем защищенности', self.armor, )
-        print('Коэффицент ловкости:', self.aagility, '\n')
-        print('Характеристики оружия', weapon.w_name, '\nБазовый урон', weapon.atk, '\nСкорость атаки', weapon.atk_speed,
-              '\nКритической урон', weapon.crit, '%', '\nШанс критического удара', weapon.crit_rate, '%\n\n')
+        print('Овации для сегодняшнего героя', self.hs['name'], str(randint(80,90)) + '-го уровня')
+        print('Оружие:', weapon.ws['name'])
+        print('Уровень здоровья:', self.hs['health'])
+        print('С уровнем защищенности', self.hs['armor'])
+        print('Сила героя:', self.hs['all_atk'])
+        print('Масса героя:', self.hs['all_weight'])
+        print('Коэффицент ловкости:', self.hs['all_agility'], '\n')
+        print('Характеристики оружия', weapon.ws['name'], '\nМасса:', weapon.ws['weight'], '\nБазовый урон:',
+              weapon.ws['atk'],
+              '\nСкорость атаки:', weapon.ws['atk_speed'], '\nКритической урон:', weapon.ws['crit'], '%',
+              '\nШанс критического удара:',
+              weapon.ws['crit_rate'], '%\n\n')
 
     # Функция атаки
     def attack(self, knight):
-        print('Нанесен удар! Герой', self.name, 'атаковал', knight.name, 'с силой удара', self.apower, '\n')
+        print('Нанесен удар! Герой', self.hs['name'], 'атаковал', knight.hs['name'], 'с силой удара', self.hs['all_atk'], '\n')
         # Подсчет вероятности крит удара
         ch = randint(1, 100)
-        if ch >= self.crit_rate:
+        if ch >= self.hs['crit_rate']:
             # Крит не прошел
-            knight.health -= self.apower - knight.armor
-            print('Герой', knight.name, 'принял удар!\nУдар оставил рыцарю', knight.health, 'здоровья!\n')
+            knight.hs['health'] -= self.hs['all_atk'] - knight.hs['armor']
+            print('Герой', knight.hs['name'], 'принял удар!\nУдар оставил рыцарю', knight.hs['health'], 'здоровья!\n')
         else:
             # Крит прошел, подсчет крита
-            damage = (self.apower - knight.armor) * 1.5 * self.crit / 100
-            knight.health -= damage
-            print(self.name, 'Наносит критический урон и нанес рыцарю', damage, 'урона!\nУдар оставил рыцарю',
-                  knight.health, 'здоровья!\n')
+            damage = (self.hs['all_atk'] - knight.hs['armor']) * 1.5 * self.hs['crit'] / 100
+            knight.hs['health'] -= damage
+            print(self.hs['name'], 'Наносит критический урон и нанес рыцарю', damage, 'урона!\nУдар оставил рыцарю',
+                  knight.hs['health'], 'здоровья!\n')
         # Просто чтобы оно не ушло сразу в конец кода
-        #input('Напишите что-нибудь, чтобы продолжить.')
+        input('Напишите что-нибудь, чтобы продолжить.')
 
     def parry(self, knight):
         # В случае непопадания в шанс удара
-        print('Рыцарь', knight.name, 'парировал атаку ' + self.name + '!')
-        #input('Напишите что-нибудь, чтобы продолжить.')
+        print('Рыцарь', knight.hs['name'], 'парировал атаку ' + self.hs['name'] + '!')
+        input('Напишите что-нибудь, чтобы продолжить.')
     # Фаза каста атака и применения. И завершение если хп кого-то < 0
 
     def fight(self, knight):
         # Условие с количеством хп
-        while self.health > 0 and knight.health > 0:
+        while self.hs['health'] > 0 and knight.hs['health'] > 0:
             # Переменная, чтобы не менять основную переменную
-            punch_chance = self.aagility
+            punch_chance = self.hs['all_agility']
             # Если ловкость упадет до 0, то ход переходит к другому
             # хотел здесь сделать систему с увелечением шанса удара при каждом промахе, но связи с побочной переменной
             # и удобством в ее использовании я отказался от этой идеи
@@ -88,17 +87,16 @@ class Hero:
                     punch_chance = 0
                 # Если избил насмерть, то досрочно закончится цикл. Да, если пройдет вторая атака, то ты ударишь труп
                 # и что?
-                if knight.health <= 0:
-                    print(knight.name, 'повержен!\nОвации рыцарю', self.name)
+                if knight.hs['health'] <= 0:
+                    print(knight.hs['name'], 'повержен!\nОвации рыцарю', self.hs['name'])
                     break
             # Я не уверен нужен ли этот break. Просто после него вроде как все заработало. Но если он не влияет ни на
-            # что, то можешь убрать. Ниже такой же
-            if knight.health <= 0:
+            # что, то можешь убрать.
+            if knight.hs['health'] <= 0:
                 break
-
             # То же самое, но для второго героя. Т.е. self поменялся с knight, теперь второй бьет первого
 
-            punch_chance = knight.aagility
+            punch_chance = knight.hs['all_agility']
             while punch_chance > 0:
                 if punch_chance > 1:
                     knight.attack(self)
@@ -110,8 +108,6 @@ class Hero:
                     else:
                         knight.parry(self)
                     punch_chance = 0
-                if self.health <= 0:
-                    print(self.name, 'повержен!\nОвации рыцарю', knight.name)
+                if self.hs['health'] <= 0:
+                    print(self.hs['name'], 'повержен!\nОвации рыцарю', knight.hs['name'])
                     break
-            if self.health <= 0:
-                break
--- weapon.py
+++ weapon.py
@@ -1,11 +1,12 @@
 # Создал класс оружия. Можно было бы приписать свойства оружия к классу героев, но это надо, чтобы позже создавать
 # множество видов оружия
 class Weapon:
-    # шабллон оружия
-    def __init__(self, w_name, w_weight, atk, atk_speed, crit, crit_rate):
-        self.w_name = w_name
-        self.w_weight = w_weight
+    # Шаблон оружия
+    def __init__(self, name, weight, atk, atk_speed, crit, crit_rate):
+        self.name = name
+        self.weight = weight
         self.atk = atk
         self.atk_speed = atk_speed
         self.crit = crit
         self.crit_rate = crit_rate
+        self.ws = {'name': self.name, 'weight': self.weight, 'atk': self.atk, 'atk_speed': self.atk_speed, 'crit': self.crit, 'crit_rate': self.crit_rate}
