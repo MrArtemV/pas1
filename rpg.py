0b96ee3aebe35d56be3f22043ce73013f110c331
Pasonik <p-ser-pan@yandex.ru>
2022-03-25 21:34:07 +0300

    New cycles for choices

--- rpg.py
+++ rpg.py
@@ -1,15 +1,19 @@
 from weapon import Weapon
 from hero import Hero
 
-# Задаю свойства оружию и героям
+# Создание классов оружия
 sword = Weapon('Рубитель времени', 5, 1600, 1.1, 125, 50)
 rapier = Weapon('Рапира "Ярость Бездны" ', 3, 450, 2.7, 70, 65)
 bow = Weapon('Лук растущей бури', 4, 600, 2.9, 0, 0)
 claymore = Weapon('Меч безжалостной волны', 14, 1900, 0.8, 175, 50)
 axe = Weapon('Топор раскола', 9, 2300, 0.9, 500, 10)
+# "Нулевые" оружия, на которые выбираются оружия
 first_weapon = Weapon()
 second_weapon = Weapon()
+# Список для перебора
+weapons = {1: sword, 2: rapier, 3: bow, 4: claymore, 5: axe}
 
+# Создание классов героев
 # Характеристики "скелета" персонажа
 # 15000 (хп), 500 (броня), 1500 (сила атаки), 0 (доп. масса), 0 (доп. ловкость), 0 (доп крит. шанс)
 knight = Hero('Иоанн Б.', 0, 0, 0, 0, 0, 0)
@@ -17,74 +21,43 @@ ninja = Hero('Си-Фу', -4000, -400, -250, -12,  1, 0)
 archer = Hero('Усолант', -2000, -155, -300, -10, 0.7, 0)
 heavy_knight = Hero('Безыменный страж', 3000, 200, 300, 15, -0.5, 0)
 student = Hero('"Некто РРР"', -2500, 300, 150, -8, -0.2, 15)
+# Герои, на которых выбираются классы
 first_knight = Hero()
 second_knight = Hero()
+# Список для перебора
+heroes = {1: knight, 2: ninja, 3: archer, 4: heavy_knight, 5: student}
 
 # Описание героев и оружия
 print('Герои:\n')
-knight.class_info()
-ninja.class_info()
-archer.class_info()
-heavy_knight.class_info()
-student.class_info()
+for i in range(1, len(heroes) + 1):
+    heroes[i].class_info()
 print('Оружия:\n')
-sword.weapon_info()
-rapier.weapon_info()
-bow.weapon_info()
-claymore.weapon_info()
-axe.weapon_info()
+for i in range(1, len(weapons) + 1):
+    weapons[i].weapon_info()
 
 # Выбор 1 героя
-name = int(input('Выбирите героя (номер по порядку)'))
-if name == 1:
-    first_knight.assignment(knight)
-elif name == 2:
-    first_knight.assignment(ninja)
-elif name == 3:
-    first_knight.assignment(archer)
-elif name == 4:
-    first_knight.assignment(heavy_knight)
-elif name == 5:
-    first_knight.assignment(student)
+name = int(input('Выбирите героя (номер по порядку)\n'))
+for i in range(1, len(heroes) + 1):
+    if i == name:
+        first_knight.assignment(heroes[i])
 
 # Выбор 1 оружия
-weapon = int(input('Выбирите оружие (номер по порядку)'))
-if weapon == 1:
-    first_weapon.assignment(sword)
-if weapon == 2:
-    first_weapon.assignment(rapier)
-if weapon == 3:
-    first_weapon.assignment(bow)
-if weapon == 4:
-    first_weapon.assignment(claymore)
-if weapon == 5:
-    first_weapon.assignment(axe)
+weapon = int(input('Выбирите оружие для 1-го героя\n'))
+for i in range(1, len(weapons) + 1):
+    if i == weapon:
+        first_weapon.assignment(weapons[i])
 
 # Выбор 2 героя
-name = int(input('Выбирите второго героя (обязательно другой герой)'))
-if name == 1:
-    second_knight.assignment(knight)
-elif name == 2:
-    second_knight.assignment(ninja)
-elif name == 3:
-    second_knight.assignment(archer)
-elif name == 4:
-    second_knight.assignment(heavy_knight)
-elif name == 5:
-    second_knight.assignment(student)
+name = int(input('Выбирите второго героя\n'))
+for i in range(1, len(heroes) + 1):
+    if i == name:
+        second_knight.assignment(heroes[i])
 
 # Выбор 2 оружия
-weapon = int(input('Выбирите оружие (обязательно другое оружие)'))
-if weapon == 1:
-    second_weapon.assignment(sword)
-if weapon == 2:
-    second_weapon.assignment(rapier)
-if weapon == 3:
-    second_weapon.assignment(bow)
-if weapon == 4:
-    second_weapon.assignment(claymore)
-if weapon == 5:
-    second_weapon.assignment(axe)
+weapon = int(input('Выбирите оружие для 2-го героя\n'))
+for i in range(1, len(weapons) + 1):
+    if i == weapon:
+        second_weapon.assignment(weapons[i])
 
 print('')
 
@@ -98,4 +71,4 @@ second_knight.print_info(second_weapon)
 
 # Бой
 first_knight.fight(second_knight)
-# Баланс. Нужен баланс
+# Баланс. Нужен баланс.
